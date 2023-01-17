f5e597d549f4be72d4533d86c16c2313e15dde9e
ejking <eric@kinghometech.com>
2023-01-17 16:12:53 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -73,7 +73,7 @@ variable "policy" {
 variable "tags" {
   description = "(Optional) A mapping of tags to assign to the bucket."
   type        = map(string)
-  default     = {}
+  default     = {"Department": "eking-dept", "Billable": "true"}
 }
 
 variable "force_destroy" {
